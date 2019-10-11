6de1245f9f8d38c42e9257ce89676293101384d1
unknown <3104340@labinfo.is.escuelaing.edu.co>
2019-10-11 18:05:44 -0500

    inico arboles

---
+++ Arenas/Arena 3/arboles.py
@@ -0,0 +1,19 @@
+from sys import stdin
+
+
+class Nodo:
+    def __init__(self):
+        self.left = None
+        self.right = None
+
+
+class Arbol:
+    def __init__(self):
+        self.raiz = None
+
+
+def main():
+    pass
+
+
+main()
